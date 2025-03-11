ec0d27d7de1f2f898dd194c825df1b27b2eb967f
Paula Meng <paulameng@MacBook-Pro-8.local>
2025-03-11 13:01:18 +0800

    fix deployment issues

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/ip-convertor' : '',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import { useState } from 'react';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Label } from '@/components/ui/label';
-import { Textarea } from '@/components/ui/textarea';
+
 import { Toaster } from '@/components/ui/sonner';
 import { toast } from 'sonner';
 import { useForm } from 'react-hook-form';
